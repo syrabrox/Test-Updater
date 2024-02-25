3d07e5993b87a47f61c18fae87f9a6c939d83f7f
AsA <102066638+syrabrox@users.noreply.github.com>
2024-02-25 16:16:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def needToUpdate(url, file_path):
             with open(file_path, 'r') as file:
                 file_data = file.read()
                 print(file_data)
-                if url_data == file_data:
+                if url_data == file_data.strip():
                     print("Data from URL matches data in the file.")
                     return False
                 else:
@@ -32,24 +32,32 @@ def needToUpdate(url, file_path):
         print("Unable to fetch data from the URL.")
         return "Fetch Error"
 
-def update(local_path, github_url):
+def update_directory_with_github_url(local_path, github_url):
+    # Files and folders to exclude from deletion
+    exclude_folders = ['.upm', '.pythonlibs']
+    exclude_files = ['.replit', 'pyproject.toml', 'poetry.lock']
+
+    # Delete everything except files and folders in the exclusion list
     for root, dirs, files in os.walk(local_path, topdown=False):
         for file in files:
-            os.remove(os.path.join(root, file))
+            if file not in exclude_files:
+                os.remove(os.path.join(root, file))
         for dir in dirs:
-            os.rmdir(os.path.join(root, dir))
+            if dir not in exclude_folders:
+                os.rmdir(os.path.join(root, dir))
 
+    # Clone the GitHub repository into the local directory
     Repo.clone_from(github_url, local_path)
+
+    print("Update completed successfully!")
 
 url_to_check = "https://raw.githubusercontent.com/syrabrox/Test-Updater/main/version.txt"
 file_path_to_check = "version.txt"
 checker = needToUpdate(url_to_check, file_path_to_check)
 downloadUrl = "https://github.com/syrabrox/Test-Updater.git"
-if checker is False:
+if checker:
   # update!
   print(checker)
   update(os.getcwd(), downloadUrl)
-elif checker is True:
-  print(checker)
 else:
   print(checker)
