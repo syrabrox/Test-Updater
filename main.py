c3d2103aa8fe45e8f48a164459a17831ad21dcd0
AsA <102066638+syrabrox@users.noreply.github.com>
2024-02-25 16:02:45 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import requests
+import os
+from git import Repo
+
+def fetch_data_from_url(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text.strip()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data from URL: {e}")
+        return None
+
+def needToUpdate(url, file_path):
+    url_data = fetch_data_from_url(url)
+
+    if url_data is not None:
+        try:
+            with open(file_path, 'r') as file:
+                file_data = file.read()
+                print(file_data)
+                if url_data == file_data:
+                    print("Data from URL matches data in the file.")
+                    return False
+                else:
+                    print("Data from URL does not match data in the file.")
+                    return True
+        except FileNotFoundError:
+            print(f"File not found at path: {file_path}")
+            return "File Error"
+    else:
+        print("Unable to fetch data from the URL.")
+        return "Fetch Error"
+
+def update(local_path, github_url):
+    for root, dirs, files in os.walk(local_path, topdown=False):
+        for file in files:
+            os.remove(os.path.join(root, file))
+        for dir in dirs:
+            os.rmdir(os.path.join(root, dir))
+
+    Repo.clone_from(github_url, local_path)
+
+url_to_check = "https://raw.githubusercontent.com/syrabrox/Test-Updater/main/version.txt"
+file_path_to_check = "version.txt"
+checker = needToUpdate(url_to_check, file_path_to_check)
+downloadUrl = "https://github.com/syrabrox/Test-Updater.git"
+if checker is False:
+  # update!
+  print(checker)
+  update(os.getcwd(), downloadUrl)
+elif checker is True:
+  print(checker)
+else:
+  print(checker)
