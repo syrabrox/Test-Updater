77447df73ccd568d3b0abb59b1985d204c8853e3
AsA <102066638+syrabrox@users.noreply.github.com>
2024-02-28 18:42:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import requests
 import os
 from git import Repo
+import sys
+import socket
+import subprocess
+from getpass import getuser
+import time 
+import shutil
+import time
+import platform
+import traceback
 
+user_name = getuser()
+startup = 'C:\\Users\\{}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'.format(user_name)
+script_path = sys.argv[0]
+destination_path = os.path.join(startup, os.path.basename(script_path))
+
+if not os.path.exists(destination_path):
+    shutil.copy(script_path, destination_path)
+    print("File copied successfully to startup folder.")
+else:
+    print("File already exists in the startup folder.")
+
+def handle_error(exception):
+  print(f"An error occurred: {exception}")
+  exc_type, exc_obj, exc_tb = sys.exc_info()
+  fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+  print(f"Error in line: {exc_tb.tb_lineno}")
+  
 def fetch_data_from_url(url):
     try:
         response = requests.get(url)
@@ -31,7 +57,7 @@ def needToUpdate(url, file_path):
         print("Unable to fetch data from the URL.")
         return "Fetch Error"
 
-def update(local_path, github_url):
+def update(local_path, github_url, exe_path):
     exclude_folders = ['.upm', '.pythonlibs']
     exclude_files = ['.replit', 'pyproject.toml', 'poetry.lock']
     for root, dirs, files in os.walk(local_path, topdown=False):
@@ -43,13 +69,27 @@ def update(local_path, github_url):
                 os.rmdir(os.path.join(root, dir))
     Repo.clone_from(github_url, local_path)
     print("Update completed successfully!")
+    try:
+      subprocess.run([exe_path])
+      print("Executable file ran successfully.")
+    except subprocess.CalledProcessError as e:
+      handle_error(e)
+
+program_files_path = os.path.join(os.environ['ProgramFiles'], 'Timeouter')
+exe_path = os.path.join(program_files_path, 'main.exe')
 
 url_to_check = "https://raw.githubusercontent.com/syrabrox/Test-Updater/main/version.txt"
-file_path_to_check = "version.txt"
+file_path_to_check = os.path.join(program_files_path, "version.txt")
 checker = needToUpdate(url_to_check, file_path_to_check)
 downloadUrl = "https://github.com/syrabrox/Test-Updater.git"
+
 if checker:
-  print(checker)
-  update(os.getcwd(), downloadUrl)
+  update(program_files_path, downloadUrl, exe_path)#os.getcwd(), downloadUrl, exe_path)
 else:
   print(checker)
+
+
+# if os.path.exists(exe_path):
+    # print(f"Path to executable: {exe_path}")
+# else:
+    # print("Executable not found in the specified path.")
